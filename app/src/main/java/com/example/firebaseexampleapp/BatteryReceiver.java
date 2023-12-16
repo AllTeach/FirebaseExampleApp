606ae4fad84e4e205161f996fb7d5f50fdf772c4
AllTeach <33581448+AllTeach@users.noreply.github.com>
2023-12-16 22:45:07 +0200

    Added LiveData

---
+++ app/src/main/java/com/example/firebaseexampleapp/BatteryReceiver.java
@@ -0,0 +1,17 @@
+package com.example.firebaseexampleapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BatteryReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context,"Battery is low, use wisely",Toast.LENGTH_SHORT).show();
+
+    }
+}
